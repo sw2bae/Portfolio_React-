0abeb0926c57d9709d7eaaaf9659613444c8240d
Sang Bae <sw2.bae@gmail.com>
2020-08-04 15:50:17 -0500

    Updates

---
+++ precache-manifest.a422b85326905de902be6df3f2148727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da76765da3a6ed2f4d60e84d7dbf9c5",
+    "url": "/Portfolio_React-/index.html"
+  },
+  {
+    "revision": "05b829935fe363d28201",
+    "url": "/Portfolio_React-/static/js/2.d22ff1b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio_React-/static/js/2.d22ff1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1825e5cbe78fe1db5c96",
+    "url": "/Portfolio_React-/static/js/main.9840a9a1.chunk.js"
+  },
+  {
+    "revision": "242ae9edddb7cd7bf6ec",
+    "url": "/Portfolio_React-/static/js/runtime-main.a155964e.js"
+  },
+  {
+    "revision": "2cb82ec76b092bf8577a3d90c48b2e17",
+    "url": "/Portfolio_React-/static/media/Profile.2cb82ec7.pdf"
+  }
+]);
