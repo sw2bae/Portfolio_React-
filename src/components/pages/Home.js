d0030c53efe9d68640837a227aaae9031404a9fb
Sang Bae <sw2.bae@gmail.com>
2020-09-15 13:46:44 -0500

    title changed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home(props) {
   return (
     <div className="container text-center">
-      <h1 className="mt-5 mb-5">Developer Wannabe</h1>
+      <h1 className="mt-5 mb-5">Full Stack Web Developer / SW QA Engineer</h1>
       <div className="card">
         <p className="text-muted mt-5 mb-5">
           <img src="https://i.ibb.co/j43CqTL/pic.jpg" alt="pic" border="0" width="40%" height="500" />
